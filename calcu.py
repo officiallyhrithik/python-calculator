5b08c6c6863a18bd387b5adb3c320ca8267b7466
Hrithikkkkk <mithrithik3027@gmail.com>
2026-09-14 19:23:47 +0530

    Update calcu.py

--- calcu.py
+++ calcu.py
@@ -2,14 +2,11 @@ print("Simple Calculator")
 
 a = float(input("Enter first number: "))
 b = float(input("Enter second number: "))
-
 print("1. Addition")
 print("2. Subtraction")
 print("3. Multiplication")
 print("4. Division")
-
 choice = input("Choose an operation: ")
-
 if choice == "1":
     print("Result:", a + b)
 elif choice == "2":
